60efff0d63cac037c1aec6a2a66cb312bf242cb6
Grzegorz Łysio <glysio16@wp.pl>
2017-05-28 23:56:02 +0200

    MyOffer

--- MiA_projekt/Models/AccountViewModels/MyOfferVM.cs
+++ MiA_projekt/Models/AccountViewModels/MyOfferVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,22 @@ namespace MiA_projekt.Models.AccountViewModels
 {
     public class MyOfferVM
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
-        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+
+        public Address Address { get; set; }
+
+        public decimal Price { get; set; }
+
+        public short GuestCount { get; set; }
+
+        public DateTime From { get; set; }
+
+        public DateTime To { get; set; }
+
 
     }
 }

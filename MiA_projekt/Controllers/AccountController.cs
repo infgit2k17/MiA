ca59fca75a5fe81cd1d50c54de1f347af460e95b
Przemyslaw Herman <ebp7@protonmail.com>
2017-05-29 17:37:16 +0200

    email verification fix

--- MiA_projekt/Controllers/AccountController.cs
+++ MiA_projekt/Controllers/AccountController.cs
@@ -133,8 +133,6 @@ namespace MiA_projekt.Controllers
                     var callbackUrl = Url.Action(nameof(ConfirmEmail), "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
                     await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
                         $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    _logger.LogInformation(3, "User created a new account with password.");
 
                     return View("ConfirmRequired");
                 }

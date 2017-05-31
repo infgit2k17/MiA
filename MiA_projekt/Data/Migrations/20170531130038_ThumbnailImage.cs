6413a12c00bd95ad942d18e947b90322d152178e
Grzegorz Łysio <glysio16@wp.pl>
2017-05-31 15:15:50 +0200

    new apartment

---
+++ MiA_projekt/Data/Migrations/20170531130038_ThumbnailImage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiA_projekt.Data.Migrations
+{
+    public partial class ThumbnailImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Thumbnail",
+                table: "Apartments",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Thumbnail",
+                table: "Apartments");
+        }
+    }
+}

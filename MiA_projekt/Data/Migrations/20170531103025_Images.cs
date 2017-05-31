199e197c09c6b74dc3540581adfcfd4b6f2c1d76
Przemyslaw Herman <ebp7@protonmail.com>
2017-05-31 12:31:02 +0200

    migration

---
+++ MiA_projekt/Data/Migrations/20170531103025_Images.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiA_projekt.Data.Migrations
+{
+    public partial class Images : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Apartments");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Apartments",
+                nullable: true);
+        }
+    }
+}
